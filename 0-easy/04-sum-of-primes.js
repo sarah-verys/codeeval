76366d460c05905f56dd1feb55bfc84fc5f926b0
Sarah Godoshian <sarah.godoshian@verys.com>
2016-09-27 21:57:54 -0700

    Modifying solution for easy challenge #4 to comply with strict mode.

--- 0-easy/04-sum-of-primes.js
+++ 0-easy/04-sum-of-primes.js
@@ -1,3 +1,4 @@
+"use strict";
 /**
  * SUM OF PRIMES
  * https://www.codeeval.com/open_challenges/4/
@@ -9,39 +10,32 @@
  *
  */
 
-class SumOfPrimes {
-  constructor () {}
-  
-  isPrime(integer) {
-    var numberToCheck = integer,
-        divisor = 3;
-    if (numberToCheck == 2) return true; // in order to start divisor at 3
-    if (numberToCheck < 2 || numberToCheck % 2 == 0) return false;
-    while(divisor * divisor <= numberToCheck) {
-      if (numberToCheck % divisor == 0) {
-        return false;
-      }
-      divisor = divisor + 2;
+var isPrime = function(integer) {
+  var numberToCheck = integer,
+      divisor = 3;
+  if (numberToCheck == 2) return true;
+  if (numberToCheck < 2 || numberToCheck % 2 == 0) return false;
+  while(divisor * divisor <= numberToCheck) {
+    if (numberToCheck % divisor == 0) {
+      return false;
     }
-    return true;
+    divisor = divisor + 2;
   }
-
-  sumFirstThousandPrimes() {
-    var primeCount = 0,
-        sumOfPrimes = 0,
-        currentNumber = 2; // 2 is the first prime number
-    while (primeCount < 1000) {
-      if (this.isPrime(currentNumber)) {
-        sumOfPrimes += currentNumber;
-        primeCount++;
-      }
-      currentNumber++;
-    }
-    console.log('Sum of first thousand prime numbers is:', sumOfPrimes);
-  }
+  return true;
 }
 
-let SumOfPrimesInstance = new SumOfPrimes();
+var sumFirstThousandPrimes = function() {
+  var primeCount = 0,
+      sumOfPrimes = 0,
+      currentNumber = 2; // 2 is the first prime number
+  while (primeCount < 1000) {
+    if (isPrime(currentNumber)) {
+      sumOfPrimes += currentNumber;
+      primeCount++;
+    }
+    currentNumber++;
+  }
+  return sumOfPrimes;
+}
 
-SumOfPrimesInstance.sumFirstThousandPrimes();
-
+console.log(sumFirstThousandPrimes());
